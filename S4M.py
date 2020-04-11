fa120d471ff58915efdba99b0f6d949255651906
viiwee <viiwee@live.com>
2020-04-10 23:40:38 -0400

    Create initial Main file

---
+++ S4M.py
@@ -0,0 +1,80 @@
+import sys
+import base64
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+import logging
+import _S4M
+import random
+
+# Begin log
+logging.basicConfig(filename='S4M-Main.log', level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.debug('Beginning of Program')
+
+# Set Constants
+matrix_width = 4
+matrix_height = 4
+matrix_salt_length = 2  # Bytes
+matrix_str_length = matrix_width * matrix_height * 2 - (matrix_salt_length * 2)  # Will give i for the length of the user
+# input, in steps of the number of hex characters in each matrix. The number of hex characters per matrix will be
+# 2*height*width - 4. The -4 takes care of the two salt bytes.
+# There are two digits for each hex character. Ex: 00 is the value for NULL
+
+# Create fake inputs
+usr_input = 'matthew fisher is a purdue student studying cybersecurity'.encode()
+key = 'cnit370'.encode()
+
+# Convert usr_input to hex
+usr_input = usr_input.hex()
+
+# Create the 16 byte key
+key = SHA256.new(key).digest()
+hashkey = key.hex()[:32]
+logging.debug('Hex key: ' + hashkey)
+
+# Create the key matrix
+k_matrix = _S4M.create_matrix(hashkey, matrix_width, matrix_height)
+logging.debug('Key Matrix: ' + str(k_matrix))
+
+# Split input into an array
+input_array = []
+for i in range(0, len(usr_input), matrix_str_length):
+
+    # Create the string that this matrix will contain
+    input_append = usr_input[i:i + matrix_str_length]
+
+    # Add padding
+    if len(input_append) < matrix_str_length:
+        diff = (matrix_str_length - len(input_append)) // 2  # Dividing by 2 so that append can append a whole hex char
+        input_append += '00' * diff  # Append 00 as padding
+
+    # Add Salt
+    for j in range(0, matrix_salt_length*2):
+        input_append += random.choice('0123456789abcdef')
+    logging.debug('Created salt: ' + input_append[28:32])  # 28:32 so that it selects the last 2 bytes
+
+    # Convert the string to a matrix
+    input_append = _S4M.create_matrix(input_append, matrix_width, matrix_height)
+
+    # Append the matrix to the list of matrices
+    input_array.append(input_append)
+logging.debug('input_array: ' + str(input_array))
+
+# Print matrix before encryption
+print(input_array)
+
+# Send each matrix for encrypting
+input_array = _S4M.encrypt_matrix(input_array, k_matrix)
+print(input_array)
+
+# Print matrix after decryption
+print(_S4M.decrypt_matrix(input_array, k_matrix))
+
+
+
+
+# For decoding
+test_hex = '72736563757269747900000000000000'
+test_string = bytearray.fromhex(test_hex)
+stripped_test = test_string.strip(b'\x00')
+print(str(stripped_test.decode()))
+
---
+++ _S4M.py
@@ -0,0 +1,185 @@
+import logging
+
+
+def create_matrix(string, width, height):
+    i = 0
+    matrix = []
+    for y in range(0, height):
+        newrow = []
+        for x in range(0, width):
+            newrow.append(string[i:i+2])
+            i += 2
+        matrix.append(newrow)
+    return matrix
+
+
+def xor_matrices(matrix1, matrix2):
+    matrix_width = len(matrix1[0])
+    matrix_height = len(matrix1[0])
+    # logging.debug('Width: ' + str(matrix_width))
+    # logging.debug('Height: ' + str(matrix_height))
+    o_matrix = matrix2
+    for y in range(0, matrix_height):
+        for x in range(0, matrix_width):
+            # logging.debug('XOR ' + str(hex(int(matrix1[y][x], 16))) + ' + ' + str(hex(int(matrix2[y][x], 16))))
+            m1 = int(matrix1[y][x], 16)  # Convert matrix 1's value to a hex value
+            m2 = int(matrix2[y][x], 16)  # Convert matrix 2's value to a hex value
+            output = hex(m1 ^ m2)[2:]  # Bitwise XOR on the values of m1 and m2
+            if len(output) < 2:  # If the hex returned a value less than 8 (in decimal),
+                # add a 0 so it is still two digits
+                output = '0' + output
+            o_matrix[y][x] = output
+    logging.debug('Finished Bitwise XOR: ' + str(o_matrix))
+    return o_matrix
+
+
+def e_switch_column(k_matrix, i_matrix):
+    logging.debug('Beginning switchColumn')
+    logging.debug('e_switch_column initial matrix: ' + str(i_matrix))
+    matrix_width = len(k_matrix[0])
+    matrix_height = len(k_matrix[0])
+    for i in range(0, matrix_height):  # Goes through each row
+        for j in range(0, matrix_width):  # Goes through each column
+            column1 = int(k_matrix[i][j][0], 16) % 4
+            column2 = int(k_matrix[i][j][1], 16) % 4
+            logging.debug('key: ' + str(i) + ',' + str(j) + ' Replace column ' + str(column1) +
+                          ' with column ' + str(column2))
+            for k in range(0, matrix_height):  # Runs the column switch on each row of the columns selected
+                # logging.debug('before: ' + str(i_matrix[k]))
+                temp = i_matrix[k][column1]
+                i_matrix[k][column1] = i_matrix[k][column2]
+                i_matrix[k][column2] = temp
+                # logging.debug('after: ' + str(i_matrix[k]))
+    logging.debug('e_switch_column final matrix: ' + str(i_matrix))
+    return i_matrix
+
+def d_switch_column(k_matrix, i_matrix):
+    logging.debug('Beginning decrypt_switchColumn')
+    logging.debug('d_switch_column initial matrix: ' + str(i_matrix))
+    matrix_width = len(k_matrix[0])
+    matrix_height = len(k_matrix[0])
+    for i in range(matrix_height - 1, -1, -1):  # Goes through each row
+        for j in range(matrix_width - 1, -1, -1):  # Goes through each column
+            column1 = int(k_matrix[i][j][0], 16) % 4
+            column2 = int(k_matrix[i][j][1], 16) % 4
+            # logging.debug('key: ' + str(i) + ',' + str(j) + ' Replace column ' + str(column1) +
+            #               ' with column ' + str(column2))
+            for k in range(0, matrix_height):  # Runs the column switch on each row of the columns selected
+                # logging.debug('before: ' + str(i_matrix[k]))
+                temp = i_matrix[k][column1]
+                i_matrix[k][column1] = i_matrix[k][column2]
+                i_matrix[k][column2] = temp
+                # logging.debug('after: ' + str(i_matrix[k]))
+    logging.debug('d_switch_column final matrix: ' + str(i_matrix))
+    return i_matrix
+
+
+def e_switch_row(k_matrix, i_matrix):
+    logging.debug('Beginning switch_row')
+    logging.debug('e_switch_row initial matrix: ' + str(i_matrix))
+    matrix_width = len(k_matrix[0])
+    matrix_height = len(k_matrix[0])
+    for i in range(0, matrix_height):  # Goes through each row
+        for j in range(0, matrix_width):  # Goes through each column
+            row1 = int(k_matrix[i][j][0], 16) % 4
+            row2 = int(k_matrix[i][j][1], 16) % 4
+            logging.debug('key: ' + str(i) + ',' + str(j) + ' Replace row ' + str(row1) +
+                          ' with row ' + str(row2))
+            for k in range(0, matrix_width):  # Runs the row switch on each column of the rows selected
+                # logging.debug('before: ' + str(i_matrix[k]))
+                temp = i_matrix[row1][k]
+                i_matrix[row1][k] = i_matrix[row2][k]
+                i_matrix[row2][k] = temp
+                # logging.debug('after: ' + str(i_matrix[k]))
+    logging.debug('e_switch_row final matrix: ' + str(i_matrix))
+    return i_matrix
+
+def d_switch_row(k_matrix, i_matrix):
+    logging.debug('Beginning decrypt_switchRow')
+    logging.debug('d_switch_row initial matrix: ' + str(i_matrix))
+    matrix_width = len(k_matrix[0])
+    matrix_height = len(k_matrix[0])
+    for i in range(matrix_height - 1, -1, -1):  # Goes through each row
+        for j in range(matrix_width - 1, -1, -1):  # Goes through each column
+            row1 = int(k_matrix[i][j][0], 16) % 4
+            row2 = int(k_matrix[i][j][1], 16) % 4
+            logging.debug('key: ' + str(i) + ',' + str(j) + ' Replace row ' + str(row1) +
+                          ' with row ' + str(row2))
+            for k in range(0, matrix_width):  # Runs the column switch on each row of the columns selected
+                # logging.debug('before: ' + str(i_matrix[k]))
+                temp = i_matrix[row1][k]
+                i_matrix[row1][k] = i_matrix[row2][k]
+                i_matrix[row2][k] = temp
+                # logging.debug('after: ' + str(i_matrix[k]))
+    logging.debug('d_switch_row final matrix: ' + str(i_matrix))
+    return i_matrix
+
+
+def e_switch_block(k_matrix, i_matrix):
+    logging.debug('Beginning e_switch_block')
+    logging.debug('e_switch_block initial matrix: ' + str(i_matrix))
+    matrix_width = len(k_matrix[0])
+    matrix_height = len(k_matrix[0])
+    for i in range(0, matrix_height):  # Goes through each row .
+        for j in range(0, matrix_width, 2):  # Goes through each column
+            column1 = int(k_matrix[i][j][0], 16) % 4
+            row1 = int(k_matrix[i][j][1], 16) % 4
+
+            column2 = int(k_matrix[i+1][j][0], 16) % 4
+            row2 = int(k_matrix[i+1][j][1], 16) % 4  ############################################BEGIN HERE#######################################
+            logging.debug('key: ' + str(i) + ',' + str(j) + ' Replace column ' + str(column1) +
+                          ' with column ' + str(column2))
+            for k in range(0, matrix_height):  # Runs the column switch on each row of the columns selected
+                # logging.debug('before: ' + str(i_matrix[k]))
+                temp = i_matrix[k][column1]
+                i_matrix[k][column1] = i_matrix[k][column2]
+                i_matrix[k][column2] = temp
+                # logging.debug('after: ' + str(i_matrix[k]))
+    logging.debug('e_switch_block final matrix: ' + str(i_matrix))
+    return i_matrix
+
+def d_switch_block(k_matrix, i_matrix):
+    logging.debug('Beginning decrypt_switchColumn')
+    logging.debug('d_switch_column initial matrix: ' + str(i_matrix))
+    matrix_width = len(k_matrix[0])
+    matrix_height = len(k_matrix[0])
+    for i in range(matrix_height - 1, -1, -1):  # Goes through each row
+        for j in range(matrix_width - 1, -1, -1):  # Goes through each column
+            column1 = int(k_matrix[i][j][0], 16) % 4
+            column2 = int(k_matrix[i][j][1], 16) % 4
+            # logging.debug('key: ' + str(i) + ',' + str(j) + ' Replace column ' + str(column1) +
+            #               ' with column ' + str(column2))
+            for k in range(0, matrix_height):  # Runs the column switch on each row of the columns selected
+                # logging.debug('before: ' + str(i_matrix[k]))
+                temp = i_matrix[k][column1]
+                i_matrix[k][column1] = i_matrix[k][column2]
+                i_matrix[k][column2] = temp
+                # logging.debug('after: ' + str(i_matrix[k]))
+    logging.debug('d_switch_column final matrix: ' + str(i_matrix))
+    return i_matrix
+
+
+def encrypt_matrix(input_array, k_matrix):
+    for i in range(0, len(input_array)):
+        logging.debug('Beginning encryption of Array ' + str(i) + ': ' + str(input_array[i]))
+        i_matrix = input_array[i]
+        for j in range(0, 20):
+            i_matrix = xor_matrices(k_matrix, i_matrix)
+            i_matrix = e_switch_column(k_matrix, i_matrix)
+            i_matrix = e_switch_row(k_matrix, i_matrix)
+            logging.debug('Matrix ' + str(i) + ', Round: ' + str(j) + ': ' + str(i_matrix))
+        input_array[i] = i_matrix
+    return input_array
+
+
+def decrypt_matrix(input_array, k_matrix):
+    for i in range(0, len(input_array)):
+        logging.debug('Beginning encryption of Array ' + str(i) + ': ' + str(input_array[i]))
+        i_matrix = input_array[i]
+        for j in range(0, 20):
+            i_matrix = d_switch_row(k_matrix, i_matrix)
+            i_matrix = d_switch_column(k_matrix, i_matrix)
+            i_matrix = xor_matrices(k_matrix, i_matrix)
+            logging.debug('Matrix ' + str(i) + ', Round: ' + str(j) + ': ' + str(i_matrix))
+        input_array[i] = i_matrix
+    return input_array
